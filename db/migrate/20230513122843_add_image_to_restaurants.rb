0cbac9b5000e3f797d29754b6a43954085ec1031
joyce lau <jshizuki@yahoo.com.hk>
2023-05-15 09:53:58 +0900

    Backend done

---
+++ db/migrate/20230513122843_add_image_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddImageToRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    add_column :flats, :image_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Address.street_name,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: Faker::Number.between(from: 50, to: 200),
+    number_of_guests: Faker::Number.between(from: 1, to: 5)
+  )
+  puts "Flat with id #{flat.id} has been created."
+end
